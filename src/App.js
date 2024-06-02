8597bc632637dd5a1efea76d99fa27e651f55a83
Aktonen <aku.tolonen1999@hotmail.fi>
2024-06-02 19:42:41 +0300

    Added routes and pages.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
 import './App.css';
+
+// Pages
+import HomePage from './pages/HomePage';
+import HireMe from './pages/HireMe';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/HireMe" element={<HireMe />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
