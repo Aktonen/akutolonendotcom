ed0d0d9cd3aac7769359193afecb376551a07083
Aktonen <aku.tolonen1999@hotmail.fi>
2024-06-02 20:54:16 +0300

    Made the Hire me button for the home page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
+// Stylesheet
+import "../Styles//HomePage.css";
+
 function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="homePage">
+      <div className="hireMeButtonWrapper">
+        <Link to="/HireMe">
+          <button className="hireMeButton">
+            Hire me
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
