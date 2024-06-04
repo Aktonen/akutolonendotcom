881d602a18613123a12e2d8dd918ccc4200cb29d
Aktonen <aku.tolonen1999@hotmail.fi>
2024-06-04 12:59:12 +0300

    Added font and some text to HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
 // Mui Components
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
 // Stylesheet
 import "../Styles//HomePage.css";
+import '@fontsource/roboto/300.css';
 
 function HomePage() {
   return (
@@ -16,6 +17,18 @@ function HomePage() {
           </Button>
         </Link>
       </div>
+      <Typography variant="h1" className="homePageTitle" align="center">
+        Welcome to my portfolio!
+      </Typography>
+      <Typography variant="h6" className="homePageSubTitle" align="center">
+        About me
+      </Typography>
+      <Typography variant="body1" className="homePageText" align="center">
+        My name is Aku Tolonen. I&apos;m an almost gratuated Bachelor of business information systems from Oulu, Finland. I have a passion for coding and I&apos;m always looking for new challenges to solve. I have experience in web development and mobile development. I&apos;m always looking for new projects to work on, so if you have a project in mind, don&apos;t hesitate to contact me!
+      </Typography>
+      <Typography variant="h6" className="homePageSubTitle" align="center">
+        My projects
+      </Typography>
     </div>
   );
 }
