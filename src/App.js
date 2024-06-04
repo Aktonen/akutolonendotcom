04d8a8ee477853f13a22d73a69f820371b7bdbdc
Aktonen <aku.tolonen1999@hotmail.fi>
2024-06-04 12:58:30 +0300

    Added footer to App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ import {
 // Pages
 import HomePage from './Pages/HomePage';
 import HireMe from './Pages/HireMe';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/HireMe" element={<HireMe />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/HireMe" element={<HireMe />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
